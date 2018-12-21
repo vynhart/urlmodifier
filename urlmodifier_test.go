c23d3d865e739fc799f34d1800a7eef726207bd5
vynhart <vynhart@gmail.com>
2018-12-21 21:22:55 +0700

    refine test

--- urlmodifier_test.go
+++ urlmodifier_test.go
@@ -8,12 +8,12 @@ func TestSubDomain(t *testing.T) {
 	myUrl := "https://s2.bukalapak.com/avt/7700597/medium/02420bf25da2677d984420e4c8479c61.jpg?go=true"
 	subdomain, err := SubDomain(myUrl)
 	if err != nil {
-		t.Errorf("Fail getting subdomain. Error: %v", err)
+		t.Errorf("Error: %v", err)
 	}
 
 	expectValue := "s2"
 	if subdomain != expectValue {
-		t.Errorf("Fail getting subdomain. Expect: %v, Got: %v", expectValue, subdomain)
+		t.Errorf("Expect: %v, Got: %v", expectValue, subdomain)
 	}
 }
 
@@ -21,12 +21,12 @@ func TestReplaceSubDomain(t *testing.T) {
 	myUrl := "https://www.bukalapak.com/avt/7700597/medium/02420bf25da2677d984420e4c8479c61.jpg?go=true"
 	newUrl, err := ReplaceSubDomain(myUrl, "s9")
 	if err != nil {
-		t.Errorf("Fail replacing subdomain. Error: %v", err)
+		t.Errorf("Error: %v", err)
 	}
 
 	expectValue := "https://s9.bukalapak.com/avt/7700597/medium/02420bf25da2677d984420e4c8479c61.jpg?go=true"
 	if newUrl != expectValue {
-		t.Errorf("Fail replacing subdomain. Expect: %v, Got: %v", expectValue, newUrl)
+		t.Errorf("Expect: %v, Got: %v", expectValue, newUrl)
 	}
 }
 
@@ -37,7 +37,7 @@ func TestRandomizeSubDomain(t *testing.T) {
 	subDomains := []string{"s1", "s2", "s3"}
 	newUrl, err := RandomizeSubDomain(myUrl, subDomains)
 	if err != nil {
-		t.Errorf("Fail randomizing subdomain. Error: %v", err)
+		t.Errorf("Error: %v", err)
 	}
 
 	newUrls := []string{
@@ -54,7 +54,7 @@ func TestRandomizeSubDomain(t *testing.T) {
 	}
 
 	if !found {
-		t.Error("Fail randomizing subdomain. Got: ", newUrl)
+		t.Error("Error: ", newUrl)
 	}
 }
 
@@ -62,11 +62,11 @@ func TestReplacePathComponent(t *testing.T) {
 	myUrl := "https://s1.bukalapak.com/avt/7700597/medium/02420bf25da2677d984420e4c8479c61.jpg?go=true"
 	newUrl, err := ReplacePathComponent(myUrl, "small", 3)
 	if err != nil {
-		t.Error("ReplacePathComponent is Fail. Error: ", err)
+		t.Error("Error: ", err)
 	}
 
 	expectValue := "https://s1.bukalapak.com/avt/7700597/small/02420bf25da2677d984420e4c8479c61.jpg?go=true"
 	if newUrl != expectValue {
-		t.Errorf("ReplacePathComponent is Fail. Expect: %v, Got: %v", expectValue, newUrl)
+		t.Errorf("Expect: %v, Got: %v", expectValue, newUrl)
 	}
 }
